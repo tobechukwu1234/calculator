bc561445e0db2f9f26f26c1ee9a86d4e54f8d156
tobechukwu1234 <tobechukwuomeje@gmail.com>
2024-09-20 21:18:34 -0700

    just for

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,21 +3,16 @@ import React, {useState} from 'react'
 function Calculator(){
 
   const [show, setShow] = useState('')
-  
-
   function displayNumber(newShow){
     setShow(n => n += newShow)
-    
   }
-
   function getNumber(event){
     const newShow = event.target.value
     displayNumber(newShow)
   }
   function deleteNumber(){
     setShow('')
-    
-  }
+}
   function oneNumber(){
     const one = show.toString().slice(0,-1)
     setShow(one)
@@ -74,9 +69,9 @@ function Calculator(){
         </form>
 
       </div>
-      <div class="end">
+      {/* <div class="end">
         <p>Created by <span>Tobechukwu Samuel Omeje</span>, August 2024</p>
-      </div>
+      </div> */}
     </div>
 
 
